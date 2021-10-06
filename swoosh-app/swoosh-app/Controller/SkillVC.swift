b715ad9003c482c11647de2588ad37b0afb4bc65
Your Nameclear <jkb6131@psu.edu>
2021-10-06 15:49:24 -0400

    added view controllers

---
+++ swoosh-app/swoosh-app/Controller/SkillVC.swift
@@ -0,0 +1,29 @@
+//
+//  SkillVC.swift
+//  swoosh-app
+//
+//  Created by Beck, James on 10/6/21.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
